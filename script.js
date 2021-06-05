45832a075ebe8624f2d4ec0d724415cad5d28e0c
BarbaraBilonic <60608361+BarbaraBilonic@users.noreply.github.com>
2021-06-05 19:10:56 +0200

    m

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+document.querySelector("#filter-btn").addEventListener("click",filterButtonClicked);
+document.querySelector("#reset-filter").addEventListener("click",resetButtonClicked);
+document.querySelector("#back").addEventListener("click",backButtonClicked);
 
 
-var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#111023"])
+var colors=d3.scale.linear().domain([500000,90000000]).range([ "#eafaf1","#14916a"])
+		
 		var year=9;
-		let country_data;
+		all=1;
+		let country_data=Array();
 		fetch("country_data.json")
 		.then(response=>{return response.json();})
 		.then(data=>dataReady(data));
@@ -11,21 +16,22 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 		let map_data;
 		var countries;	
 		var barchart;
-	    var mapWidth = 600;
-	    var mapHeight = 650;
-		var popGraphWidth=500;
-		var popGraphHeight=400;
-		var barPadding=10;
-		var barWidth=popGraphWidth/10-barPadding;
-		var pieChartWidth=600;
-		var pieChartHeight=500;
+	    var mapWidth = 430;
+	    var mapHeight = 700;
+		var popGraphWidth=700;
+		var popGraphHeight=350;
+		var barPadding=8;
+		var barWidthAllCountries;
+		var barWidthOneCountry=popGraphWidth/10-30;
+		var pieChartWidth=450;
+		var pieChartHeight=300;
 		var pieChartMargin=30;
 		var radius=120;
 		var arc=d3.svg.arc()
 			.innerRadius(80)
 			.outerRadius(radius)
 	    var projection = d3.geo.mercator()
-	    .center([5, 55])
+	    .center([10, 55])
 	    .scale(550)
        .translate([mapWidth / 2, mapHeight / 2]);
 
@@ -39,24 +45,24 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 
 		var popGraphSvg= d3.select("#population")
 		.append("svg")
-		.attr("width", popGraphWidth + 200)
-		.attr("height", popGraphHeight + 200)
+		.attr("width", popGraphWidth + 150)
+		.attr("height", popGraphHeight + 100)
 		.style("background-color", "white")
 		.append("g")
-		.attr("transform", "translate(" + 100 + "," + 100 +")");
+		.attr("transform", "translate(" + 100 + "," +0 +")");
 
 		var pieSvg=d3.select("#pie")
 			.append("svg")
 			.attr("width",pieChartWidth)
 			.attr("height",pieChartHeight)
 			.append("g")
-			.attr("transform","translate("+pieChartWidth/2+","+pieChartHeight/2+")")
+			.attr("transform","translate("+120+","+pieChartHeight/2+")")
 		
 		
 			
-		var xScalepopGraph=d3.scale.ordinal()
-							.domain([2010,2011,2012,2013,2014,2015,2016,2017,2018,2019])
-							.rangeRoundBands([0,popGraphWidth],.5);
+		var xScaleYears=d3.scale.ordinal()
+			.domain([2010,2011,2012,2013,2014,2015,2016,2017,2018,2019])
+			.rangeRoundBands([0,popGraphWidth],.5);
 		
 		var barchartDiv=d3.select("#population").append("div")
 						.attr("class","tootlip")
@@ -72,10 +78,85 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 			
  
 	function dataReady(data){
-		country_data=data;
+		data.forEach(element => {
+			country_data.push(
+				{
+					country_name: element.country_name,
+					country_code:element.country_code,
+					isIncluded:1,
+					population:element.population,
+					urban_population:element.urban_population,
+					rural_population:element.rural_population
+				}
+			)
+		});
 		createMap();
+		document.querySelector("#lower").value=d3.min(country_data.map(function(d){return d3.min(d.population.map(function(e){return e;}))}));
+		document.querySelector("#lower").min=d3.min(country_data.map(function(d){return d3.min(d.population.map(function(e){return e;}))}));
+		document.querySelector("#upper").value=d3.max(country_data.map(function(d){return d3.max(d.population.map(function(e){return e;}))}));
+		document.querySelector("#upper").max=d3.max(country_data.map(function(d){return d3.max(d.population.map(function(e){return e;}))}));
+		lower=d3.min(country_data.map(function(d){return d3.min(d.population.map(function(e){return e;}))}));
+		upper=d3.max(country_data.map(function(d){return d3.max(d.population.map(function(e){return e;}))}));
+		document.querySelector("#lower-min-p").innerHTML=`Min (${lower})`;	
+		document.querySelector("#lower-max-p").innerHTML=`Max (${upper})`;
 							
 						}
+	function backButtonClicked(){
+		all=1;
+		createGraph();
+		document.querySelector("#country_name").innerHTML=`<h3>European Union Countries<h3>`
+		year=9;
+		filterCountries();
+		document.querySelector("#year").innerHTML=`<h3>${years[year]}</h3>`;
+		removePieChart();
+	}
+	function filterButtonClicked(){
+		
+		upper=document.querySelector("#upper").value;
+		lower=document.querySelector("#lower").value;
+		
+		filterCountries();
+		
+		colors.domain([lower,upper]);
+		console.log(all);
+		if(all===1){
+		createGraph();
+		
+					}
+		changeMap(year);					
+									
+		}
+
+		function resetButtonClicked(){
+			document.querySelector("#lower").value=d3.min(country_data.map(function(d){return d3.min(d.population.map(function(e){return e;}))}));
+			document.querySelector("#upper").value=d3.max(country_data.map(function(d){return d3.max(d.population.map(function(e){return e;}))}));
+			upper=document.querySelector("#upper").value;
+			lower=document.querySelector("#lower").value;
+			filterCountries();
+			
+			colors.domain([lower,upper]);
+		
+			if(all===1){
+			createGraph();
+			
+		}
+		changeMap(year);
+	}
+					
+		function filterCountries(){
+		
+		var i;
+			for(i=0;i<country_data.length;i++){
+				
+				if(country_data[i].population[year]<lower || country_data[i].population[year]>upper){
+					country_data[i].isIncluded=0;
+					
+				}
+				else{
+					country_data[i].isIncluded=1;
+				}
+			}			
+			}	
   
 
 	function createMap(){
@@ -92,8 +173,8 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 	    .style("stroke-width", 1)
 	    .style("stroke-opacity", function(d){;return getOpacity(d.properties.adm0_a3);})
 		.on("mouseover", function(d){
-			
-			if(getCountryData(d.properties.adm0_a3)!=null){
+			var c=getCountryData(d.properties.adm0_a3);
+			if(c!=null && c.isIncluded===1){
 			d3.select(this).style("fill","#eeffc4");
 			d3.select(this).transition()
 				.duration(50)
@@ -110,7 +191,8 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 			
 		})
 		.on("mouseout",function(d){
-			if(getCountryData(d.properties.adm0_a3)!=null){
+			var c=getCountryData(d.properties.adm0_a3);
+				if(c!=null && c.isIncluded===1){
 				d3.select(this).style("fill",function(d){return getColor(d.properties.adm0_a3,9)});
 				d3.select(this).transition()
 				.duration(50)
@@ -122,22 +204,41 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 		})
 		.on("click",function(d){
 			var country=getCountryData(d.properties.adm0_a3);
-			if(country!=null){
+			if(country!=null && country.isIncluded===1){
+				if(all===1){
+					createCountryBarchart(country);
+					createPieChart(country.country_code,year)
+				}else{
 				changeBarchart(country);
-				updatePieChart(country.country_code,9)
+				updatePieChart(country.country_code,year)
+				}
+				
 			}
 		});
 		map_data=eu;
 	    })
-       createGraph("EUU");
-	  createPieChart("EUU",9);
+       createGraph();
+	  
 	}
+
 
 	
 
+	function highlightCountry(countryCode){
+		countries
+			.data(map_data.features)
+			.style("fill",function(d){
+				if(d.properties.adm0_a3===countryCode) return "#eeffc4";
+				else return getColor(d.properties.adm0_a3,year);
+			});
+
+
+	}
+
+
 	function changeBarchart(country){
 		var yScalepopGraph=d3.scale.linear()
-			.domain([(d3.min(country.population.map(function(d){return d;}))-50000),d3.max(country.population.map(function(d){return d;}))])
+			.domain([0,d3.max(country.population.map(function(d){return d;}))])
 			.range([popGraphHeight,0]);
 		
 		var popGraphYAxis=d3.svg.axis()
@@ -147,46 +248,31 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 		popGraphSvg.selectAll(".yaxis").call(popGraphYAxis)
 
 		barchart
-		.data(country.population)
-		.attr("x",function(d,i){return xScalepopGraph(years[i]);})	
+		.data(country.population.map(function(d){return [d,country.country_code]}))
+		.attr("x",function(d,i){return xScaleYears(years[i]);})	
 		.attr("y",300)
 		.attr("height", 100);
 
 		popGraphSvg.selectAll("rect")
 				.transition()
 				.duration(800)
-				.attr("y", function(d){return yScalepopGraph(d);})
-				.attr("height",function(d){return popGraphHeight-yScalepopGraph(d); })
+				.attr("y", function(d){return yScalepopGraph(d[0]);})
+				.attr("height",function(d){return popGraphHeight-yScalepopGraph(d[0]); })
 				.delay(function(d,i){console.log(i) ; return(i*100)})
 		
-	}
-
-	function changeMap(index){
-		countries
-			.data(map_data.features)
-			.style("fill", function(d){return getColor(d.properties.adm0_a3,index);})
-			.on("mouseout",function(d){
-				if(getCountryData(d.properties.adm0_a3)!=null){
-					d3.select(this).style("fill",function(d){return getColor(d.properties.adm0_a3,index)});
-					d3.select(this).transition()
-					.duration(50)
-					.attr("opacity",1);
-					mapDiv.transition()
-					.duration(50)
-					.style("opacity",0);
-					}
-			});
 		
 	}
-	function createGraph(countryCode){
-		var country=getCountryData(countryCode);
 
+	function createCountryBarchart(country){
+		all=0;
+		
+		popGraphSvg.selectAll("*").remove();
 		var yScalepopGraph=d3.scale.linear()
-			.domain([(d3.min(country.population.map(function(d){return d;}))-50000),d3.max(country.population.map(function(d){return d;}))])
+			.domain([0,d3.max(country.population.map(function(d){return d;}))])
 			.range([popGraphHeight,0]);
 
 		var popGraphXAxis=d3.svg.axis()
-					.scale(xScalepopGraph)
+					.scale(xScaleYears)
 					.orient("bottom");
 
 		var popGraphYAxis=d3.svg.axis()
@@ -212,13 +298,13 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 			.attr("transform","rotate(-90)");
 		
 		 barchart = popGraphSvg.selectAll("rect")
-			.data(country.population)
+			.data(country.population.map(function(d){return [d,country.country_code]}))
 			.enter()
 			.append("rect")
-			.attr("x",function(d,i){return xScalepopGraph(years[i]);})	
+			.attr("x",function(d,i){return xScaleYears(years[i]);})	
 			.attr("y",300)
-			.attr("height", 100)
-			.attr("width",barWidth)
+			.attr("height", 0)
+			.attr("width",barWidthOneCountry)
 			.attr("fill","#7bafc7")
 			.on("mouseover", function(d,i){
 				d3.select(this).attr("fill","#b8e1f5");
@@ -228,7 +314,7 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 				barchartDiv.transition()
 					.duration(50)
 					.style("opacity",1);
-				let num=d;
+				let num=d[0];
 				barchartDiv.html(num)
 					.style("left",(d3.event.pageX+10)+"px")
 					.style("top",(d3.event.pageY-15)+"px")
@@ -242,23 +328,153 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 					.duration(50)
 					.style("opacity",0);
 			})
-			.on("click",function(d,i){changeMap(i); updatePieChart(countryCode,i); year=i;});
+			.on("click",function(d,i){
+				changeMap(i); year=i;
+				document.querySelector("#year").innerHTML=`<h3>${years[year]}</h3>`;
+				console.log(d[1]);
+				updatePieChart(d[1],i);
+
+			 });
 		
 			popGraphSvg.selectAll("rect")
 				
 				.transition()
-				.duration(800)
-				.attr("y", function(d){return yScalepopGraph(d);})
-				.attr("height",function(d){return popGraphHeight-yScalepopGraph(d); })
-				.delay(function(d,i){ return(i*100)})
+				.duration(1000)
+				.attr("y", function(d){return yScalepopGraph(d[0]);})
+				.attr("height",function(d){return popGraphHeight-yScalepopGraph(d[0]); })
+				.delay(function(d,i){ return(i*100*2)})
 
+	}
+
+	function changeMap(index){
+		filterCountries();
+		countries
+			.data(map_data.features)
+			.style("fill", function(d){return getColor(d.properties.adm0_a3,index);})
+			.on("mouseout",function(d){
+				if(getCountryData(d.properties.adm0_a3)!=null){
+					d3.select(this).style("fill",function(d){return getColor(d.properties.adm0_a3,index)});
+					d3.select(this).transition()
+					.duration(50)
+					.attr("opacity",1);
+					mapDiv.transition()
+					.duration(50)
+					.style("opacity",0);
+					}
+			});
+		
+	}
+	function createGraph(){
+		all=1;
+		var c=country_data.filter(element=>element.isIncluded===1)
+		if(c.length<5){
+		barWidthAllCountries=popGraphWidth/c.length-150;
+		}
+		else if(c.length<8){
+			barWidthAllCountries=popGraphWidth/c.length-70;
+		}
+		else{
+			barWidthAllCountries=popGraphWidth/c.length-barPadding;
+		}
+		popGraphSvg.selectAll("*").remove();
+		var c=country_data.filter(
+			element=>element.isIncluded===1);
+			console.log(c);
+		console.log("bbbbbbbbb");
+		var yScale=d3.scale.linear()
+			.domain([0,d3.max(c.map(function(d){return d.population[9];}))])
+						.range([popGraphHeight,0]);
+		var xScale=d3.scale.ordinal()
+		.domain(c.map(function(d){return d.country_name;}))
+						.rangeRoundBands([0,popGraphWidth],.5);
+		console.log(xScale);
+		var popGraphXAxis=d3.svg.axis()
+					.scale(xScale)
+					.orient("bottom");
+
+		var popGraphYAxis=d3.svg.axis()
+						.scale(yScale)
+						.orient("left")
+						.ticks(4);
+
+		popGraphSvg.append("g")
+			.attr("class","xaxis")
+			.attr("transform","translate(0,"+popGraphHeight+")")
+			.call(popGraphXAxis)
+			.selectAll("text")
+			.style("text-anchor", "end")
+			.attr("transform","rotate(-45)");
+		
+			
+		popGraphSvg.append("g")
+			.attr("class", "yaxis")
+			.call(popGraphYAxis)
+			.append("text")
+			.style("text-anchor","end")
+			.attr("x",0)
+			.attr("y",0)
+			.text("population")
+			.attr("transform","rotate(-90)");
+		
+		 barchart = popGraphSvg.selectAll("rect")
+			.data(c)
+			.enter()
+			.append("rect")
+			.attr("x",function(d){return xScale(d.country_name);})	
+			.attr("y",0)
+			.attr("height", 0)
+			.attr("width",barWidthAllCountries)
+			.attr("fill","#7bafc7")
+			.on("mouseover", function(d,i){
+				d3.select(this).attr("fill","#b8e1f5");
+				d3.select(this).transition()
+					.duration(50)
+					.attr("opacity",0.85);
+				barchartDiv.transition()
+					.duration(50)
+					.style("opacity",1);
+				let num=d.country_name+", population: "+d.population[9];
+				barchartDiv.html(num)
+					.style("left",(d3.event.pageX)+"px")
+					.style("top",(d3.event.pageY)+"px")
+				highlightCountry(d.country_code);
+			})
+			.on("mouseout",function(d,i){
+				d3.select(this).attr("fill","#7bafc7");
+				d3.select(this).transition()
+					.duration(50)
+					.attr("opacity",1);
+				barchartDiv.transition()
+					.duration(50)
+					.style("opacity",0);
+				changeMap(9);
+			})
+			.on("click",function(d,i){ changeMap(i);
+				document.querySelector("#country_name").innerHTML=`<h3>${d.country_name}</h3>`;
+				 createCountryBarchart(d);
+				createPieChart(d.country_code,i);});
+		
+			popGraphSvg.selectAll("rect")
+				
+				.transition()
+				.duration(1000)
+				.attr("y", function(d){return yScale(d.population[9]);})
+				.attr("height",function(d){return popGraphHeight-yScale(d.population[9]); })
+				.delay(function(d,i){ return(i*100)})
 
 		
 		
 	}
+	function removePieChart(){
+		pieSvg.selectAll("*").remove();
+		
+		document.querySelector("#pie_title").innerHTML=`<h3></h3>`;
+	}
 	function updatePieChart(countryCode,index){
+			console.log(countryCode);
 			chart.remove();
 			var country=getCountryData(countryCode);
+			document.querySelector("#pie_title").innerHTML=`Percentage of rural and urban population for ${country.country_name} in ${years[year]}`;
 			var pie=d3.layout.pie();
 			var data=pie([country.urban_population[index],country.rural_population[index]]);
 			chart=pieSvg.selectAll("path")
@@ -274,8 +490,8 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 					.outerRadius(radius)
 				)
 			.attr("fill", function(d,i){
-				if(i===0) return "yellow";
-				else return "green";
+				if(i===0) return "#83e6c4";
+				else return "#fca956";
 			});
 	chart
 			.on("mouseover",function(d,i){
@@ -306,6 +522,7 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 	function createPieChart(countryCode,index){
 		
 		var country=getCountryData(countryCode);
+		document.querySelector("#pie_title").innerHTML=`Percentage of rural and urban population for ${country.country_name} in ${years[year]}`;
 		var pie=d3.layout.pie();
 		var data=pie([country.urban_population[index],country.rural_population[index]]);
 		chart=pieSvg.selectAll("path")
@@ -318,8 +535,8 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 				.duration(1000)
 				.attr("d",arc)
 				.attr("fill", function(d,i){
-					if(i===0) return "yellow";
-					else return "green";
+					if(i===0) return "#83e6c4";
+					else return "#fca956";
 				})
 	
 		
@@ -346,8 +563,8 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 					.style("opacity",0);
 			});		
 	
-		pieSvg.append("circle").attr("cx",150).attr("cy",0).attr("r", 6).style("fill", "yellow")
-		pieSvg.append("circle").attr("cx",150).attr("cy",30).attr("r", 6).style("fill", "green")
+		pieSvg.append("circle").attr("cx",150).attr("cy",0).attr("r", 6).style("fill", "#83e6c4")
+		pieSvg.append("circle").attr("cx",150).attr("cy",30).attr("r", 6).style("fill", "#fca956")
 		pieSvg.append("text").attr("x", 160).attr("y", 5).text("Urban population (%)").style("font-size", "15px").attr("alignment-baseline","middle")
 		pieSvg.append("text").attr("x", 160).attr("y", 35).text("Rural population (%)").style("font-size", "15px").attr("alignment-baseline","middle")
 
@@ -359,13 +576,13 @@ var colors=d3.scale.linear().domain([500000,90000000]).range([ "#dddcef","#11102
 	
 	function getOpacity(countryCode){
 		let country=getCountryData(countryCode);
-		if(country!=null){return 1;}
+		if(country!=null && country.isIncluded===1){return 1;}
 		else return 0;
 	}
 
 		function getColor(countryCode,index){
 			let country=getCountryData(countryCode);
-			if(country!=null){
+			if(country!=null && country.isIncluded===1){
 				var color= colors(country.population[index]);
 				return color;
 			}
